ee751d450cd79b089eb89bfee128e7c9ec33af17
hieuchu4869 <chuhieu457@gmail.com>
2023-05-17 15:21:59 +0700

    java28tech

---
+++ Bai_1.java
@@ -0,0 +1,14 @@
+
+package Buoi_1;
+
+
+public class Bai_1 {
+        public static void main(String[] args) {
+                int a;
+                long chuvi;
+                float dientich;
+                System.out.println("");
+                  
+                
+        }
+}
